27f650011ced9e924ae22fb6ab50af8f94fd1283
pythonwby <pythonwu0511@qq.com>
2021-08-15 14:34:28 +0800

    0

--- algorithm1-2/P1271.cpp
+++ algorithm1-2/P1271.cpp
@@ -2,39 +2,37 @@
    P1271 [深基9.例] 选举学生会
  * @Author: 乌搏猿
  * @Date: 2021-08-15 11:03:47
- * @LastEditTime: 2021-08-15 11:21:47
- * @state: Other
+ * @LastEditTime: 2021-08-15 14:34:13
+ * @state: AC
  * @FilePath: \code_for_luogu\algorithm1-2\P1271.cpp
  */
 //暂未完成！！！
 #include <cstdio>
 
 //n:数据最大值,m:数据量,a:读入数组
-int n,m,a[2000010];
+int n,m,a[2000010],tmp[2000010];
 
-//合并函数
-void hb(int *l,int llen,int *r,int rlen){
-    int temp[2000010],i,j,k;
-    while(i<llen&&j<rlen){
-        if(l[i]<r[j]) temp[k++]=l[i++];
-        else if(r[j]<l[i]) temp[k++]=r[j++];
-    }
-    while(i<llen) temp[k++]=l[i++];
-    while(j<rlen) temp[k++]=r[j++];
-    for(i=0;i<k;i++) l[i]=temp[i];
-}
 //归并排序
-void gp(int *a,int len){
-    if(len<2) return;
-    int mid=len/2;
-    gp(a,mid);
-    gp(a+mid,len-mid);
-    hb(a,mid,a+mid,len-mid);
+void gp(int q[],int l,int r){
+    if (l >= r) return;
+
+    int mid = l + r >> 1;
+
+    gp(q, l, mid), gp(q, mid + 1, r);
+
+    int k = 0, i = l, j = mid + 1;
+    while (i <= mid && j <= r)
+        if (q[i] <= q[j]) tmp[k ++ ] = q[i ++ ];
+        else tmp[k ++ ] = q[j ++ ];
+    while (i <= mid) tmp[k ++ ] = q[i ++ ];
+    while (j <= r) tmp[k ++ ] = q[j ++ ];
+
+    for (i = l, j = 0; i <= r; i ++, j ++ ) q[i] = tmp[j];
 }
-int main(){
+int main_1271(){
     scanf("%d%d",&n,&m);
     for(int i=0;i<m;i++) scanf("%d",&a[i]);
-    gp(a,m);
+    gp(a,0,m-1);
     for(int i=0;i<m;i++) printf("%d ",a[i]);
     system("pause");
     return 0;
