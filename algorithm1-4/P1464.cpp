ce7a84a73545f93df11142ba4054a9ef76a13515
pythonwby <pythonwu0511@qq.com>
2021-08-17 13:05:31 +0800

    0

--- algorithm1-4/P1464.cpp
+++ algorithm1-4/P1464.cpp
@@ -2,7 +2,7 @@
    P1464 Function
  * @Autor: ÎÚ²«Ô³
  * @Date: 2021-08-16 12:48:03
- * @LastEditTime: 2021-08-16 17:02:20
+ * @LastEditTime: 2021-08-17 12:07:19
  * @state: Other
  * @FilePath: \code_for_luogu\algorithm1-4\P1464.cpp
  */
@@ -10,7 +10,12 @@
 #include <cstdio>
 #define ll long long
 
+//sum:debug val
+int sum;
+
 ll w(ll a,ll b,ll c){
+    sum++;
+    printf("%d\n",sum);
     if(a<=0||b<=0||c<=0) return 1;
     if(a>20||b>20||c>20) return w(20,20,20);
     if(a<b&&b<c) return w(a,b,c-1)+w(a,b-1,c-1)+w(a,b-1,c);
@@ -19,12 +24,11 @@ ll w(ll a,ll b,ll c){
 int main(){
     while(true){
         ll a,b,c;
-        scanf("%d%d%d",&a,&b,&c);
+        scanf("%lld%lld%lld",&a,&b,&c);
         if(a==-1&&b==-1&&c==-1) break;
         ll d=w(a,b,c);
-        printf("w(%d, %d, %d) = %d",a,b,c,d);
+        printf("w(%lld, %lld, %lld) = %d",a,b,c,d);
     }
-    
     system("pause");
     return 0;
 }
