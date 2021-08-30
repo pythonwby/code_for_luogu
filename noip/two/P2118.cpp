8ebd8b11232866532368231e3513f5ba8eaca742
pythonwby <pythonwu0511@qq.com>
2021-08-30 18:43:09 +0800

    0

--- noip/two/P2118.cpp
+++ noip/two/P2118.cpp
@@ -2,7 +2,7 @@
    P2118 [NOIP2014 普及组] 比例简化
  * @Autor: 乌搏猿
  * @Date: 2021-08-30 18:14:53
- * @LastEditTime: 2021-08-30 18:33:36
+ * @LastEditTime: 2021-08-30 18:43:05
  * @state: Other
  * @FilePath: \code_for_luogu\noip\two\P2118.cpp
  */
@@ -14,9 +14,5 @@ using namespace std;
 int a,b,c;
 
 int main(){
-    scanf("%d%d%d",&a,&b,&c);
-    printf("%d %d\n",a/__gcd(a,b),b/__gcd(a,b));
     
-    system("pause");
-    return 0;
 }
