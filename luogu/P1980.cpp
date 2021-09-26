8ae9770712f99aea427a408f6d89a9c3f0b15ff8
pythonwby <pythonwu0511@qq.com>
2021-09-26 20:31:43 +0800

    0

---
+++ luogu/P1980.cpp
@@ -0,0 +1,25 @@
+/*
+   P1980 [NOIP2013 普及组] 计数问题
+ * @Autor: 乌搏猿
+ * @Date: 2021-09-26 20:24:15
+ * @LastEditTime: 2021-09-26 20:31:19
+ * @state: AC
+ * @FilePath: \code_for_luogu\luogu\P1980.cpp
+ */
+#include <cstdio>
+
+int n,x,ans=0;
+
+int main_1980(){
+    scanf("%d%d",&n,&x);
+    for(int i=1;i<=n;i++){
+        int tmp=i;
+        while(tmp>0){
+            if(tmp%10==x) ans++;
+            tmp/=10;
+        }
+    }
+    printf("%d\n",ans);
+    system("pause");
+    return 0;
+}
