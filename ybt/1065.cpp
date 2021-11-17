71ebf8fa52df9a14dc267816002d57758a0b9199
pythonwby <pythonwu0511@qq.com>
2021-11-17 20:01:02 +0800

    0

--- ybt/1065.cpp
+++ ybt/1065.cpp
@@ -2,7 +2,15 @@
    1065£ºÆæÊýÇóºÍ
  * @Autor: ÎÚ²«Ô³
  * @Date: 2021-11-17 19:58:59
- * @LastEditTime: 2021-11-17 19:59:00
+ * @LastEditTime: 2021-11-17 20:00:56
  * @state: Other
  * @FilePath: \code_for_luogu\ybt\1065.cpp
  */
+#include <cstdio>
+
+int n,m,ans;
+
+int main(){
+    scanf("%d%d",&m,&n);
+    
+}
